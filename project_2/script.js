e3bffe53077133636ed78e584968cdf9d890ae83
SardorSDeV <sardor_zolotoy@mail.ru>
2020-03-27 16:27:36 +0500

    Bizning uchinchi commitimiz

---
+++ project_2/script.js
@@ -0,0 +1,49 @@
+'use strict';
+let a = 5;
+console.log(a);
+/*alert(1);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+var obj = {};
+console.log(4*0);
+console.log('string'*9);
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[2]);
+console.log(leftBorderWidth);*/
+
+//alert("Hello world!");
+
+//let answer = confirm("Are you here?");
+//console.log(answer);
+//let answer = +prompt("Yoshingiz 18 dami?", "Ha");
+//console.log(typeof(answer));
+
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose);
